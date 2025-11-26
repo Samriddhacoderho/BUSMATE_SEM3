aae603e3ad803a862cf58cd9c903a49ef14a00c8
Samriddha R. Satyal <satyalsamriddha@gmail.com>
2025-11-26 09:44:04 +0545

    Test Commit

--- app/src/main/java/com/example/busmate/view/ParentDashboardActivity.kt
+++ app/src/main/java/com/example/busmate/view/ParentDashboardActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,12 +18,26 @@ class ParentDashboardActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            ParentDashboardActivityUI()
         }
     }
 }
 
 @Composable
 fun ParentDashboardActivityUI() {
-    
+    Scaffold() {paddingValues ->
+        LazyColumn(Modifier
+            .fillMaxSize()
+            .padding(paddingValues)) {
+            item {
+                Text(text = "Parent Dashboard")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ParentDashboardActivityUIPreview() {
+    ParentDashboardActivityUI()
 }
