f325cfb7680c76bc6b192c4232adb47b59b7052a
Samriddha R. Satyal <satyalsamriddha@gmail.com>
2025-12-02 19:29:47 +0545

    Model completed, repo structure completed for Support

---
+++ app/src/main/java/com/example/busmate/model/SupportModel.kt
@@ -0,0 +1,9 @@
+package com.example.busmate.model
+
+data class SupportModel(
+    val uid: String,
+    val name: String,
+    val typeofUser:String,
+    val title:String,
+    val message:String
+)
