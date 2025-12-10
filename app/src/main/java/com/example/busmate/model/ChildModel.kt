c150db51f7288010f3d9f9eba76fa21940790050
keshabbhattarai126 <keshab126bhattarai@gmail.com>
2025-12-10 23:52:51 +0545

    added child model,viewmodel,interface,Impl and view

---
+++ app/src/main/java/com/example/busmate/model/ChildModel.kt
@@ -0,0 +1,26 @@
+package com.example.busmate.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ChildModel (
+    val firstName: String = "",
+    val lastName: String = "",
+    val studentId: String = "", // This must remain the unique identifier
+    val busRouteId: String = "",
+    val pickUpLocation: String = "",
+    val dropOffLocation: String = ""
+    // 'parentUid' is removed as it's implicit in the Parent's document path
+) : Parcelable {
+    fun toMap(): Map<String, Any> {
+        return mapOf(
+            "firstName" to firstName,
+            "lastName" to lastName,
+            "studentId" to studentId,
+            "busRouteId" to busRouteId,
+            "pickUpLocation" to pickUpLocation,
+            "dropOffLocation" to dropOffLocation
+        )
+    }
+}
