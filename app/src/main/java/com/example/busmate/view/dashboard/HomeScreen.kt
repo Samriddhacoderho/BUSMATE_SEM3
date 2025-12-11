ab1b2cf8d718f57df0269ceb0489fce93c3a6f6f
keshabbhattarai126 <keshab126bhattarai@gmail.com>
2025-12-12 02:19:20 +0545

    minor fix

--- app/src/main/java/com/example/busmate/view/dashboard/HomeScreen.kt
+++ app/src/main/java/com/example/busmate/view/dashboard/HomeScreen.kt
@@ -31,6 +31,7 @@ import com.example.busmate.ui.theme.BusMateBlue
 import com.example.busmate.ui.theme.BusMateGreen
 import com.example.busmate.ui.theme.BusMateOrange
 import com.example.busmate.view.AddChildActivity
+import com.example.busmate.view.BusScreen
 
 
 @Composable
@@ -44,6 +45,10 @@ fun HomeScreen() {
 
     val navigateToAddChild: () -> Unit = {
         val intent = Intent(context, AddChildActivity::class.java)
+        context.startActivity(intent)
+    }
+    val navigateToAddBus: () -> Unit = {
+        val intent = Intent(context, BusScreen::class.java)
         context.startActivity(intent)
     }
 
@@ -78,7 +83,7 @@ fun HomeScreen() {
                     imageResource = if(model?.typeofUser=="Parent") R.drawable.girl else if (model?.typeofUser=="Driver") R.drawable.schoolbus else R.drawable.driver,
                     mapImageResource = R.drawable.map
                 )
-                if(model?.typeofUser=="Parent" || model?.typeofUser=="Driver") NotificationsAlertHeaderScreen() else NotificationsAlertHeaderAdmin()
+                if(model?.typeofUser=="Parent" || model?.typeofUser=="Driver") NotificationsAlertHeaderScreen() else NotificationsAlertHeaderAdmin(onAddBusClick = navigateToAddBus)
             }
         }
     }
@@ -403,7 +408,7 @@ fun WelcomeCardAdmin(adminName: String){
 }
 
 @Composable
-fun NotificationsAlertHeaderAdmin(){
+fun NotificationsAlertHeaderAdmin(onAddBusClick: () -> Unit){
     Row(modifier = Modifier.fillMaxWidth().padding(start = 16.dp, top = 16.dp, end = 5.dp),
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.SpaceBetween
@@ -415,7 +420,7 @@ fun NotificationsAlertHeaderAdmin(){
             color = Color.Black.copy(alpha = 0.8f)
         )
         OutlinedButton(
-            onClick = {},
+            onClick = onAddBusClick,
             shape = RoundedCornerShape(20.dp),
             colors = ButtonDefaults.outlinedButtonColors(
                 contentColor = BusMateBlue
@@ -434,7 +439,7 @@ fun NotificationsAlertHeaderAdmin(){
             )
 
             Spacer(modifier = Modifier.width(4.dp))
-            Text("Create Notification", fontSize = 14.sp)
+            Text("Add Bus", fontSize = 14.sp)
         }
     }
     Spacer(modifier = Modifier.height(10.dp))
